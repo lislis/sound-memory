1da2c48ad5369e47f95f26ff0e446a732de0a161
lislis <mail@lislis.de>
2025-12-15 14:50:48 +0100

    play with plugins

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use bevy::prelude::*;
+use bevy_midi::prelude::*;
+
+mod plugins;
+use plugins::PlayerPlugin;
 
 fn main() {
-
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(PlayerPlugin)
         .run();
 }
+
